14bfcb7d4c1bdeadcec739ddbc449fff7c4a27f3
Talha <03345238002h@gmail.com>
2024-05-05 18:22:53 +0500

    bg change according mobile/pc

--- Notify/src/Components/Home.jsx
+++ Notify/src/Components/Home.jsx
@@ -3,11 +3,14 @@ import Navbar from './Navbar'
 import Sidebar from './Sidebar'
 
 function Home() {
+
+    const screenSize = window.innerWidth;
+
     return (
         <>
             
             <div className="img">
-                <img className=' w-5/6 h-[90svh]  absolute bottom-0 right-0 lg:w-11/12' src="/assets/bg.jpg" alt="Notes image" />
+                <img className=' w-5/6 h-[90svh]  absolute bottom-0 right-0 lg:w-11/12' src={screenSize<640? "/assets/bg4.jpg" : "/assets/bg.jpg"} alt="Notes image" />
                 <div className='absolute '></div>
             </div>
            
